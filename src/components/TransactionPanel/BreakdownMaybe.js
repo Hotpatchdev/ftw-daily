75229996f301de0394bf9ac7f2e04ca7dddd4abb
Arham Abeer <127412341+arhamabeer-braincrop@users.noreply.github.com>
2024-08-08 14:44:47 +0500

    Update BreakdownMaybe.js

--- src/components/TransactionPanel/BreakdownMaybe.js
+++ src/components/TransactionPanel/BreakdownMaybe.js
@@ -1,31 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
 import { DATE_TYPE_DATE, DATE_TYPE_DATETIME, LINE_ITEM_DAY, HOURLY_PRICE } from '../../util/types';
 import { ensureListing } from '../../util/data';
-import { BookingBreakdown } from '../../components';
-
+import { BookingBreakdown, PrimaryButton } from '../../components';
+import { types as sdkTypes } from '../../util/sdkLoader';
 import css from './TransactionPanel.module.css';
+import { FormattedMessage } from 'react-intl';
+import LineItemDiscountMaybe from '../BookingBreakdown/LineItemDiscountMaybe';
+import FieldDiscount from '../../forms/BookingTimeForm/FiledDiscount';
 
 // Functional component as a helper to build BookingBreakdown
 const BreakdownMaybe = props => {
-  const { className, rootClassName, breakdownClassName, transaction, transactionRole, unitType, promocode } = props;
+  const {
+    className,
+    rootClassName,
+    breakdownClassName,
+    transaction,
+    transactionRole,
+    unitType,
+    promocode,
+    intl,
+    onSumbitBookingRequestEnquiry,
+  } = props;
+  const [planType, setPlanType] = useState(null);
+  const [result, setResult] = React.useState({
+    _sdkType: 'Money',
+    amount: 0,
+    currency: 'GBP',
+  });
+  var dataToSend = {};
+
+  useEffect(() => {
+    const _publicDataPlan = transaction?.attributes?.protectedData.planType || null;
+    setPlanType(_publicDataPlan);
+    if (_publicDataPlan !== 'price') {
+      dataToSend = {
+        bookingDates: {
+          startDate: new Date(startDate),
+          endDate: new Date(endDate),
+        },
+        enquirybookingType: _publicDataPlan,
+        fromWhere: 'enquiry',
+      };
+    } else if (_publicDataPlan === 'price') {
+      dataToSend = {
+        enquirybookingType: _publicDataPlan,
+        fromWhere: 'enquiry',
+        bookingStartDate: { date: new Date(startDate) },
+        bookingEndDate: { date: new Date(endDate) },
+        bookingStartTime: startTime?.toString(),
+        bookingEndTime: endTime?.toString(),
+      };
+    }
+  }, [transaction]);
+
+  const { startTime, endTime, displayStartDate, displayEndDate, startDate, endDate } =
+    transaction?.attributes?.protectedData || {};
   const loaded = transaction && transaction.id && transaction.booking && transaction.booking.id;
   const listingAttributes = ensureListing(transaction.listing).attributes;
   const timeZone =
     loaded && listingAttributes.availabilityPlan
       ? listingAttributes.availabilityPlan.timezone
       : 'Etc/UTC';
-  const bookingType = transaction &&
-                  transaction.attributes &&
-                  transaction.attributes.protectedData &&
-                  transaction.attributes.protectedData.type || HOURLY_PRICE;
+  const bookingType =
+    (transaction &&
+      transaction.attributes &&
+      transaction.attributes.protectedData &&
+      transaction.attributes.protectedData.type) ||
+    HOURLY_PRICE;
   const dateType = bookingType === HOURLY_PRICE ? DATE_TYPE_DATETIME : DATE_TYPE_DATE;
-  
+
   const classes = classNames(rootClassName || css.breakdownMaybe, className);
   const breakdownClasses = classNames(breakdownClassName || css.breakdown);
-  
-console.log('breakdown:', transaction,transaction?.attributes,transaction?.attributes?.protectedData)
-  
+
+  console.log(
+    'breakdown maybe',
+    transaction.attributes.protectedData
+  );
+
   if (
     transaction?.attributes?.lastTransition &&
     transaction.attributes.lastTransition === 'transition/enquire' &&
@@ -33,6 +85,12 @@ console.log('breakdown:', transaction,transaction?.attributes,transaction?.attri
     Object.keys(transaction.attributes.protectedData).length > 0
     // && transactionRole === 'provider'
   ) {
+
+    console.log(
+    'breakdown maybe inside if',
+    transaction.attributes.protectedData
+  );
+    
     const formatDate = timestamp => {
       const date = new Date(timestamp);
       const options = { weekday: 'short', hour: 'numeric', minute: 'numeric', hour12: true };
@@ -175,7 +233,7 @@ console.log('breakdown:', transaction,transaction?.attributes,transaction?.attri
     _formattedDate.setDate(_formattedDate.getDate() - 1);
     let formattedDate1dayLessEndDate = _formattedDate.toISOString().split('T')[0];
 
-    // console.log('breakdown maybe =>', formattedDate1dayLessEndDate);
+    console.log('breakdown maybe =>', formattedDate1dayLessEndDate);
     const updateResult = data => {
       setResult(data);
     };
@@ -311,7 +369,7 @@ console.log('breakdown:', transaction,transaction?.attributes,transaction?.attri
     );
   }
 
-return loaded ? (
+  return loaded ? (
     <div className={classes}>
       <BookingBreakdown
         promocode={promocode}
